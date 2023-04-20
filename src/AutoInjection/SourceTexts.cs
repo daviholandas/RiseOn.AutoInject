b5f8e32b287d30f40397595930f94856dfcebff7
Davi Holanda <daviholandas@gmail.com>
2023-04-20 15:41:16 -0300

    [feat]: create the source text.

--- src/AutoInjection/SourceTexts.cs
+++ src/AutoInjection/SourceTexts.cs
@@ -15,4 +15,35 @@ public static class SourceTexts
                 public InjectServiceAttribute(ServiceLifetime serviceLifetime,
                     Type implementationType) {}
             }";
+
+    public static string ServiceCollectionExtensionSourceText()
+    {
+        /*context.RegisterImplementationSourceOutput(context.CompilationProvider, (context, compilation) =>
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("using System;");
+            sb.AppendLine("using Microsoft.Extensions.DependencyInjection;");
+            sb.AppendLine();
+            sb.AppendLine("namespace AutoInjection");
+            sb.AppendLine("{");
+            sb.AppendLine("    public static class ServiceCollectionExtension");
+            sb.AppendLine("    {");
+            sb.AppendLine("        public static IServiceCollection AddAutoInject(this IServiceCollection services)");
+            sb.AppendLine("        {");
+            foreach (var type in compilation.Assembly.GlobalNamespace.GetNamespaceTypes())
+            {
+                if (type.GetAttributes().Any(x => x.AttributeClass?.Name == "InjectServiceAttribute"))
+                {
+                    sb.AppendLine($"            services.AddTransient<{type.Name}>();");
+                }
+            }
+            sb.AppendLine("            return services;");
+            sb.AppendLine("        }");
+            sb.AppendLine("    }");
+            sb.AppendLine("}");
+            context.AddSource("ServiceCollectionExtension", SourceText.From(sb.ToString(), Encoding.UTF8));
+        });*/
+
+        return string.Empty;
+    }
 }
