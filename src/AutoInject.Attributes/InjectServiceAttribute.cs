d2803048d65eb11a6d3f7ca0f0d0e9c4f702c7f5
Davi Holanda <daviholandas@gmail.com>
2025-05-16 23:28:52 -0300

    feat: Enhance InjectServiceAttribute with CollectionName and CollectionNamespace properties; update related service registrations

--- src/AutoInject.Attributes/InjectServiceAttribute.cs
+++ src/AutoInject.Attributes/InjectServiceAttribute.cs
@@ -22,11 +22,18 @@
             bool injectAlone = false) { }
 
         /// <summary>
+        /// Gets or sets the name of the collection used to group services marked with the attribute.
+        /// This property allows categorization of services within the dependency injection container,
+        /// enabling more organized management of service registrations.
+        /// </summary>
+        public string? CollectionName { get; set; }
+
+        /// <summary>
         /// Gets or sets the namespace associated with the service class to be registered in the dependency injection container.
         /// This property allows specifying the namespace of the service to provide additional context or enable namespace-based
         /// filtering when managing service registrations.
         /// </summary>
-        public string? ServicesNameSpace { get; set; }
+        public string? CollectionNamespace { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the service interface or base class that the marked class implements or inherits from.
@@ -34,13 +41,6 @@
         /// or base class, allowing for explicit association in the dependency graph.
         /// </summary>
         public System.Type? ImplementationOf { get; set; }
-
-        /// <summary>
-        /// Gets or sets the name of the collection used to group services marked with the attribute.
-        /// This property allows categorization of services within the dependency injection container,
-        /// enabling more organized management of service registrations.
-        /// </summary>
-        public string? CollectionName { get; set; }
 
         /// <summary>
         /// Gets or sets the unique key associated with the service registration.
